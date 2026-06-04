50c1e869e78dcfd8c41340a14d59b9dfe9862e6e
Lewin Kelly <heli@odex.cf>
2026-06-04 20:48:55 +0100

    Some fixes for frontend building

--- Front/src/routes/+layout.svelte
+++ Front/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import "@skeletonlabs/skeleton/themes/theme-crimson.css"
-	import "@skeletonlabs/skeleton/styles/all.css"
+	// import "@skeletonlabs/skeleton/styles/all.css"
 	import "../app.postcss"
 	import {
 		AppShell,
--- Front/svelte.config.js
+++ Front/svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from "@sveltejs/adapter-node"
-import preprocess from "svelte-preprocess"
+import { sveltePreprocess } from "svelte-preprocess"
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
@@ -7,7 +7,7 @@ const config = {
 		adapter: adapter(),
 	},
 	preprocess: [
-		preprocess({
+		sveltePreprocess({
 			postcss: true,
 		}),
 	],
